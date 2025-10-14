60bfbf721f58b4987c3e7ee9d02aa9d116f52274
AxRoma <ananev.roma.07@bk.ru>
2025-10-14 18:52:17 +0000

    something

---
+++ lab1/src/info_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "=== System Information ==="
+echo "1. Current directory: $(pwd)"
+echo "2. Current date and time: $(date)"
+echo "3. PATH variable: $PATH"
+echo "=========================="
